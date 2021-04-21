bfbf6421f8cfb0252d353c3383630c39aedda1fe
promehedi <mmhasan563@gmail.com>
2021-04-22 03:58:58 +0600

    Connect with MongoDB

--- server/server.js
+++ server/server.js
@@ -1,26 +1,33 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
+// Enable .env
 dotenv.config()
 
+// Enable MongoDB Connection
+connectDB()
+
 const app = express()
+
+// Define Variable
+const API_URL = process.env.API_URL || '/api/v1'
+const PORT = process.env.PORT || 5000
+const NODE_ENV = process.env.NODE_ENV
 
 app.get('/', (req, res) => {
   res.send('API is running..')
 })
 
-app.get('/api/v1/products', (req, res) => {
+app.get(`${API_URL}/products`, (req, res) => {
   res.json(products)
 })
 
-app.get('/api/v1/products/:id', (req, res) => {
+app.get(`${API_URL}/products/:id`, (req, res) => {
   const product = products.find((prod) => prod._id === req.params.id)
   res.json(product)
 })
-
-const PORT = process.env.PORT || 5000
-const NODE_ENV = process.env.NODE_ENV
 
 app.listen(
   PORT,
