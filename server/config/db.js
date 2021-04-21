feef91fce570f43c4e394d17e0f895b275c767a2
promehedi <mmhasan563@gmail.com>
2021-04-22 04:01:31 +0600

    Add colors for console output

--- server/config/db.js
+++ server/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     })
 
-    console.log(`MongoDB Connected: ${connect.connection.host}`)
+    console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline)
   } catch (error) {
-    console.error(`Error: ${error.message}`)
+    console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -31,5 +32,5 @@ app.get(`${API_URL}/products/:id`, (req, res) => {
 
 app.listen(
   PORT,
-  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
