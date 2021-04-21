d42e491b954c3df4f7dca1885a3f3eee73775700
promehedi <mmhasan563@gmail.com>
2021-04-21 17:55:06 +0600

    Add Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome!</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome!</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
---
+++ client/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer className='footer-wrap'>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; 2021. All right reserverd PROSHOP. Developed by{' '}
+            <a href='http://promehedi.com' target='_blank' rel='noreferrer'>
+              Mehedi Hasan
+            </a>
+          </Col>
+        </Row>
+      </Container>
+      <h1>Footer</h1>
+    </footer>
+  )
+}
+
+export default Footer
