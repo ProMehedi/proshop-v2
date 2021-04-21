fc717eb687ffa2d9bd9c8538ca223e3291738fbd
promehedi <mmhasan563@gmail.com>
2021-04-21 19:30:27 +0600

    Rating Component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   const { _id, name, image, price, rating, numReviews } = product
   return (
-    <Card className='mb-4 rounded'>
+    <Card className='product mb-4 rounded'>
       <a href={`/product/${_id}`}>
         <Card.Img src={image} variant='top' />
 
         <Card.Body>
-          <Card.Text as='h4'>${price}</Card.Text>
+          <Card.Text as='h4' className='text-primary'>
+            ${price}
+          </Card.Text>
 
           <Card.Title>
             <strong>{name}</strong>
           </Card.Title>
 
           <Card.Text as='div'>
-            {rating} from {numReviews} Reviews
+            <Rating rating={rating} text={`from ${numReviews} Reviews`} />
           </Card.Text>
         </Card.Body>
       </a>
