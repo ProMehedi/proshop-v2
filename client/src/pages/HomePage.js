bd110890560ccb3c719dc7bcb829a7c7494fb8c5
promehedi <mmhasan563@gmail.com>
2021-04-30 03:13:06 +0600

    Message/Alert Component

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,6 +3,7 @@ import { Col, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts } from '../actions/productActions'
 import Loader from '../components/Loader'
+import Message from '../components/Message'
 import Product from '../components/Product'
 
 const HomePage = () => {
@@ -19,7 +20,7 @@ const HomePage = () => {
   }
 
   if (error) {
-    return <h1>{error}</h1>
+    return <Message variant='danger'>{error}</Message>
   }
 
   return (
