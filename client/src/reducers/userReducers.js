80e880886185a2fdcd239d237b75854adc896738
promehedi <mmhasan563@gmail.com>
2021-05-05 21:56:08 +0600

    User Login Reducer & Actions

---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,20 @@
+import * as USER from '../constants/userConstants'
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER.USER_LOGIN_REQUEST:
+      return { loading: true }
+
+    case USER.USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+
+    case USER.USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload }
+
+    case USER.USER_LOGOUT:
+      return {}
+
+    default:
+      return state
+  }
+}
--- client/src/store.js
+++ client/src/store.js
@@ -6,19 +6,26 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
 const cartItemsFromLocal = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromLocal = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const inialState = {
   cart: { cartItems: cartItemsFromLocal },
+  userLogin: { userInfo: userInfoFromLocal },
 }
 
 const middleware = [thunk]
