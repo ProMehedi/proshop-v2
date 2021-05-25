82e52ced5da327627030f7c6ca477d11bf51c725
promehedi <mmhasan563@gmail.com>
2021-05-25 21:34:28 +0600

    Create & Update Product Endpoint

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -37,3 +37,48 @@ export const deleteProductById = asyncHandler(async (req, res) => {
     throw new Error('Product not found!')
   }
 })
+
+// @desc    Create Product
+// @route   POST /api/v1/products/
+// @access  Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    user: req.user._id,
+    image: 'https://picsum.photos/840/510',
+    brand: 'Sample Brand',
+    category: 'Sample Category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample Description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+// @desc    Update Product by ID
+// @route   PUT /api/v1/products/:id
+// @access  Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body
+
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    ;(product.name = name),
+      (product.price = price),
+      (product.description = description),
+      (product.image = image),
+      (product.brand = brand),
+      (product.category = category),
+      (product.countInStock = countInStock)
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product Not Found!')
+  }
+})
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,19 +1,22 @@
 import express from 'express'
 import {
+  createProduct,
   deleteProductById,
   getProductById,
   getProducts,
+  updateProduct,
 } from '../controllers/productController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 // Products Actions
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 // Product Actions by ID
 router
   .route('/:id')
   .get(getProductById)
+  .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProductById)
 
 export default router
